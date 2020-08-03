eeef0e88c33eedc0d34627febf3119bac5197fb8
naoya umezawa <zawawa@naoyanoMacBook-Pro.local>
2020-08-04 00:58:29 +0900

    init

---
+++ D415/test.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pyrealsense2 as rs
+import cv2
+
+pipeline = rs.pipeline()
+
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 360, rs.format.bgr8, 30)
+
+profile = pipeline.start(config)
+
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        if not depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        distance = depth_image * depth_scale
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        images = np.hstack((color_image, depth_colormap))
+
+        cv2.imshow('example', images)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+finally:
+    pipeline.stop()
